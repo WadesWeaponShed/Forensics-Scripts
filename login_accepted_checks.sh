b96dcfad45df42f498f180c98fe37f1fafd4fd32
WadesWeaponShed <aforester@silverdogstudios.com>
2024-05-31 14:39:12 -0500

    Create login_accepted_checks.sh

---
+++ login_accepted_checks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+file="/var/log"
+
+# Input log file
+cat $file/secure* |grep -w "Accepted" >log.log
+logfile="log.log"
+
+# Extract hostname from the log file
+hostname=$(awk '{print $5; exit}' "$logfile")
+
+# Output CSV file with hostname in the filename
+csvfile="${hostname}_Accepted_login.csv"
+
+
+# Add header row
+awk '{
+    date=$1 " " $2 " " $3
+    time=$4
+    host=$5
+    user=$10
+    ip=$12
+    printf "%s,%s,%s,%s,%s\n", date, time, host, user, ip
+}' "$logfile" > "$csvfile"
+
+echo "CSV file generated: $csvfile"
